00d9ef56367ca96178ef1593d3834b7f43a5149b
Francesco Lazzarelli <lazzamilan.fl@gmail.com>
2021-03-24 08:54:39 +0100

    fixata addEmployees()
    da fixare la rimozione

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 var ID = 0;
 
-$( document ).ready(function() {   
+$( document ).ready(function() {  
+  $("#add-container").hide(); 
   loadTable(); 
   getData();
   addEmployees();
@@ -9,12 +10,12 @@ $( document ).ready(function() {
 
 //function used to show input for add new employee
 function showAddParameters() {
-  $("#container").append('<div><input type="text" id="first-name" placeholder="first name" class="form-control w-25 m-2 "><input type="text" id="last-name" placeholder="last name" class="form-control w-25 m-2"><input type="email" id="email" placeholder="email" class="form-control w-25 m-2"><input type="text" id="phone" placeholder="phone" class="form-control w-25 m-2"><input type="button" class="btn btn-primary m-2" id="btn-add" value="Add"></div>');
+  $("#add-container").show();
 }
 
 // function used to create the table structure on the page laod 
 function loadTable () {
-  $("#container").append('<table class="table-secondary" id="table"><tr><th colspan="3">Manage Employees</th><th colspan="2"><button class="btn btn-primary" onclick="showAddParameters()">Add New Employees</button></th></tr><tr><td>Name</td><td>Surname</td><td>Email</td><td colspan="2">Phone</td></tr></table>');
+  $("#container").append('<table id="table"><tr><th colspan="3">Manage Employees</th><th colspan="2"><button class="btn btn-primary" onclick="showAddParameters()">Add New Employees</button></th></tr><tr><td>Name</td><td>Surname</td><td>Email</td><td colspan="2">Phone</td></tr></table>');
 }
 
 // function to get all the data from db
@@ -23,7 +24,6 @@ function getData(){
       url: 'http://localhost:8080/api/tutorial/1.0/employees',
       type: 'get',
       contentType: 'application/json',
-      data: JSON.stringify(),
       accept: "*/*",
       success: function(data){
         for (let i = 0; i < data.length; i++) { 
@@ -56,6 +56,23 @@ function addEmployees(){
       accept: '*/*',
       success: function(){
         alert("employee insert correctly");
+
+        $.ajax({ // get request to refresh the table
+          url: 'http://localhost:8080/api/tutorial/1.0/employees',
+          type: 'get',
+          contentType: 'application/json',
+          accept: "*/*",
+          success: function(data){
+            $('#table tbody > tr > td').remove();
+            for (let i = 0; i < data.length; i++) { 
+              ID += 1;
+              $('#table').append("<tr><td>" + data[i]["firstName"] + "</td><td>"+ data[i]["lastName"] + "</td><td>" + data[i]["email"] + "</td><td>"+ data[i]["phone"] + "</td><td><button class=\"btn btn-warning btn-delete\" id=\"" + data[i]["employeeId"] + "\">REMOVE</button></tr>");    
+            } 
+          },
+          error: function(errorThrown){
+            console.log( errorThrown );
+          }
+        });
       },
       error: function(errorThrown){
         console.log( errorThrown );
@@ -64,7 +81,7 @@ function addEmployees(){
   });
 }
 
-// function to remove an employess from db
+// function to remove an employees from db
 function removeEmployees(){
   $(".btn-delete").click( function() {   
     var id =  $('.btn-delete').attr("id");
@@ -76,7 +93,7 @@ function removeEmployees(){
       data: JSON.stringify(),
       accept: "*/*",
       success: function(){
-        console.log( 'employees remove correctly' );
+        console.log( 'employee remove correctly' );
       },
       error: function(errorThrown){
         console.log( errorThrown );
